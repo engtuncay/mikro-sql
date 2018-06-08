772c9e04c98252bcaf3c315f8d5576dfaf2c258b
Tuncay From Winmac <tunc270@gmail.com>
2018-06-08 16:02:13 +0300

    up

--- Entegre Kullanılan Sorgular/sqlCheckAnacariByCariKodAndSektorKodu.sql
+++
@@ -1,9 +0,0 @@
-﻿
-DECLARE @_cari_kod as varchar(20) ='MK047C'
-DECLARE @_cari_Ana_cari_kodu AS varchar(20) ='MK049'
-DECLARE @_cari_sektor_kodu AS varchar(20) ='NESTLE'
-DECLARE @_cha_tarihiBas AS date ='20180501'
-DECLARE @_cha_tarihiSon AS date ='20180531'
-
-SELECT count(*) as nCountTr FROM [CARI_HESAPLAR] WITH (NOLOCK) 
-WHERE cari_Ana_cari_kodu=@_cari_kod and cari_Ana_cari_kodu<>cari_kod and cari_sektor_kodu=@_cari_sektor_kodu
